91257785c0c8a3c10d89b213f89e6a8e820c32b7
Lan <admin@lanol.cn>
2020-05-22 23:44:34 +0800

    新增退回功能。和版本更新通知

--- Main.py
+++ Main.py
@@ -1,5 +1,7 @@
 import os
 
+import requests
+
 from Brainstorm import main as brainstorm
 from Exam_Answer import main as examans
 from Get_Stu_Info import get_info
@@ -8,38 +10,54 @@ from Homework_Time import main as retime
 from Re_Grade import Main as regrade
 from Retroactive import main as buqian
 from Sign_Auto import main as autosign
+from rejectHomework import main as tui
 
-stuId = get_info()
-print("【欢迎使用职教云综合助手】")
-print(" www.lanol.cn     By:Lan")
-print("【1】职教云签到监控功能")
-print("【2】职教云签到改命功能")
-print("【3】职教云作业改分功能")
-print("【4】职教云作业改时功能")
-print("【5】头脑风暴改评分功能")
-print("【6】作业 答案 查询功能")
-print("【7】考试 答案 查询功能")
-print("【8】 退 出 当 前 账 号")
-tool = input("请输入功能序号：")
-if tool == '1':
-    autosign(stuId)
-elif tool == '2':
-    buqian(stuId)
-elif tool == '3':
-    regrade(stuId)
-elif tool == '4':
-    retime(stuId)
-elif tool == '5':
-    brainstorm(stuId)
-elif tool == '6':
-    homeanswer(stuId)
-elif tool == '7':
-    examans(stuId)
-elif tool == '8':
-    if os.path.exists('config.info'):
-        os.remove('config.info')
-        print("注销成功，回车后退出")
-        input()
-    else:
-        print("您当前没有登录！")
-        input()
+url = 'https://www.lanol.cn/zjypackage.json'
+info = requests.get(url).json()
+infomation = info['information']
+print(infomation['message'])
+name = info['name']
+new_version = info['version']
+now_version = '1.5'
+if now_version != new_version:
+    print(f"版本有更新！目前版本{now_version}，最新版本{new_version}，请前往www.lanol.cn获取最新版")
+    input("回车退出！")
+else:
+    print(f'随机一句：{requests.get("https://v1.hitokoto.cn/?encode=text&charset=utf8").text}')
+    stuId = get_info()
+    print(f"【欢迎使用{name}】")
+    print(" www.lanol.cn     By:Lan")
+    print("【1】职教云签到监控功能")
+    print("【2】职教云签到改命功能")
+    print("【3】职教云作业改分功能")
+    print("【4】职教云作业改时功能")
+    print("【5】头脑风暴改评分功能")
+    print("【6】职教云作业答案功能")
+    print("【7】职教云考试答案功能")
+    print("【8】职教云作业退回功能")
+    print("【0】 退 出 当 前 账 号")
+    tool = input("请输入功能序号：")
+    if tool == '1':
+        autosign(stuId)
+    elif tool == '2':
+        buqian(stuId)
+    elif tool == '3':
+        regrade(stuId)
+    elif tool == '4':
+        retime(stuId)
+    elif tool == '5':
+        brainstorm(stuId)
+    elif tool == '6':
+        homeanswer(stuId)
+    elif tool == '7':
+        examans(stuId)
+    elif tool == '8':
+        tui(stuId)
+    elif tool == '0':
+        if os.path.exists('config.info'):
+            os.remove('config.info')
+            print("注销成功，回车后退出")
+            input()
+        else:
+            print("您当前没有登录！")
+            input()
---
+++ rejectHomework.py
@@ -0,0 +1,36 @@
+import requests
+
+from Get_All_Course import get_all_course
+from Get_Homework_Grade import get_homework_grade
+from Get_Homework_List import get_homework_list
+
+
+def main(stuId):
+    allcourse = get_all_course(stuId)
+    openClassId = allcourse['openClassId']
+    courseOpenId = allcourse['courseOpenId']
+    homeworklist = get_homework_list(stuId, openClassId, courseOpenId)
+    if homeworklist == 0:
+        print("获取失败")
+    else:
+        homeworkTermTimeId = homeworklist['homeworkTermTimeId']
+        homeworkId = homeworklist['homeworkId']
+        grades = get_homework_grade(openClassId, homeworkId, stuId, homeworkTermTimeId)
+        if grades != 0:
+            index = 1
+            for i in grades:
+                print(f"【{index}】时间：{i['dateCreated']}\t分数{i['getScore']}")
+                index += 1
+            target = int(input("请输入要修改的序号：")) - 1
+            homeworkStuId = grades[target]['homeworkStuId']
+        else:
+            input("回车键后退出")
+    teaId = input("请输入教师ID")
+    url = 'https://zjyapp.icve.com.cn/newmobileapi/homework/rejectHomework'
+    data = {
+        'homeworkStuId': homeworkStuId,
+        'teaId': teaId,
+    }
+    result = requests.post(url=url, data=data).json()
+    print(result['msg'])
+    input("回车退出！")
